c2920c7041fbfcdd73ea956a8c3279481161f705
Quouou <benedicto.christopheeer@gmail.com>
2026-04-15 20:25:40 +0800

    added deduplication

--- scraper.py
+++ scraper.py
@@ -18,28 +18,28 @@ try:
     author_names = ['Albert Einstein']
 
     select_author = Select(driver.find_element(By.ID, "author"))
-    print(f"Amount of authors: {len(select_author.options) - 1}")
+    # print(f"Amount of authors: {len(select_author.options) - 1}")
     # for option in select_author.options:
     #     if option.text != "----------":
     #         author_names.append(option.text)
     # print('Finished collecting author names')
 
     for author in author_names:
-        select_author = Select(driver.find_element(By.ID, "author"))
-        select_author.select_by_visible_text(author)
+        selectAuthor = Select(driver.find_element(By.ID, "author"))
+        selectAuthor.select_by_visible_text(author)
 
         time.sleep(1)
 
         tags_dropdown = Select(driver.find_element(By.ID, "tag"))
-        current_author_tags = []
+        currentAuthorTags = []
         for tag in tags_dropdown.options:
             if tag.text != "----------":
-                current_author_tags.append(tag.text)
-        # print(f"Finished collecting tags for author: {author} - Tags found: {current_author_tags}")
+                currentAuthorTags.append(tag.text)
+        # print(f"Finished collecting tags for author: {author} - Tags found: {currentAuthorTags}")
 
-        for tag in current_author_tags:
-            tags_dropdown = Select(driver.find_element(By.ID, "tag"))
-            tags_dropdown.select_by_visible_text(tag)
+        for tag in currentAuthorTags:
+            tagsDropDown = Select(driver.find_element(By.ID, "tag"))
+            tagsDropDown.select_by_visible_text(tag)
 
             time.sleep(1)
 
@@ -49,11 +49,25 @@ try:
 
             content = driver.find_elements(By.CLASS_NAME, "quote")
             for q in content:
-                master_quotes.append({
-                    "author": author,
-                    "tag": tag,
-                    "quote": q.find_element(By.CLASS_NAME, "content").text
-                })
-        print (f"Finished collecting quotes for author: {author} - All Quotes: {master_quotes}")
+                quote_text = q.find_element(By.CLASS_NAME, "content").text
+                isDuplicate = False
+
+                for saved_quote in master_quotes:
+                    if saved_quote["quote"] == quote_text:
+                        saved_quote["tag"].append(tag)
+                        isDuplicate = True
+                        break
+                
+                if not isDuplicate:
+                    master_quotes.append({
+                            "author": author,
+                            "tag": tag,
+                            "quote": q.find_element(By.CLASS_NAME, "content").text
+                        
+                    })
+        print(f"Finished collecting quotes for author: {author}")
+
+    print (f"All Quotes: {master_quotes}")
+
 finally:
     driver.quit()
